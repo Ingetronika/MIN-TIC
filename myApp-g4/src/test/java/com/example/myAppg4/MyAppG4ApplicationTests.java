566efedf6d0bea39545eb015cda6489f5752369e
Ingetronika <95087048+Ingetronika@users.noreply.github.com>
2021-11-26 10:25:21 -0500

    Add files via upload

---
+++ myApp-g4/src/test/java/com/example/myAppg4/MyAppG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myAppg4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
